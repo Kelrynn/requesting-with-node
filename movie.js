9394cd019ea0a4fd73b2876d89eba1316eff5ab6
Kelrynn <cameroncastells@gmail.com>
2017-10-03 16:05:14 -0600

    adding request handling

---
+++ movie.js
@@ -0,0 +1,12 @@
+const keys = require('./env.js');
+let request = require('request');
+
+const API = `https://www.googleapis.com/customsearch/v1?key=${keys.key}&cx=${keys.id}&q=`;
+
+function get(movieTitle){
+	request(API+movieTitle+"+site%3Aimdb.com", function(error, response, body){
+		console.log(JSON.parse(body).items[0].snippet);
+	});
+}
+
+module.exports = get;
